afd72b38796263febb5f2ad60ca79951b77b0fd2
bannsec <whootandahalf@gmail.com>
2019-08-13 15:13:22 -0400

    Exception bins

---
+++ tests/linux/bins/exceptions.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+
+
+void do_good() {
+    printf("Good\n");
+}
+
+void do_access_violation() {
+    int x[1];
+    printf("%d", x[0x123123123]);
+}
+
+void do_access_read_violation() {
+    // Attempt to load from memory 0x666
+    __asm ("add 0x666,%rax");
+}
+
+void do_access_write_violation() {
+    // Attempt to load from memory 0x666
+    __asm ("add %rax, 0x666");
+}
+
+int do_abort() {
+    return raise(SIGABRT);
+}
+
+int do_ill() {
+    return raise(SIGILL);
+}
+
+// TODO: Test this... Right now, frida will hang on catching the exception, then the script will timeout and return
+// The second time it's run, it will come back correctly. Probably a bug...
+// Also, process exit will hang due to some part of this bug.
+void do_overflow(char *s) {
+    char vuln[4];
+    strcpy(vuln, s); 
+}
+
+void do_arithmetic() {
+    __asm ("xor %rax, %rax;" 
+           "div %rax");
+}
+
+int main() {
+    printf("Main");
+
+}
