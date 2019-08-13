1607f3c3bf2dfc4cffb5e38d8f82bb7dc8d3b4d5
bannsec <whootandahalf@gmail.com>
2019-08-13 00:14:09 -0400

    Testing access violation memory read

--- tests/linux/test_native_exceptions.py
+++ tests/linux/test_native_exceptions.py
@@ -87,3 +87,25 @@ def test_access_violation():
 
     # If we handled exception correctly, process should still be in good state
     assert not isinstance(do_good, frida_util.native_exception.NativeException)
+
+def test_access_violation_read():
+
+    do_access_read_violation = p.memory[p.modules['exceptions'].symbols['do_access_read_violation']]
+    do_good = p.memory[p.modules['exceptions'].symbols['do_good']] 
+
+    e = do_access_read_violation()
+    assert isinstance(e, frida_util.native_exception.NativeException)
+    str(e)
+    repr(e)
+    assert e.type == 'access-violation'
+    # This abort is run by throwing the signal from libc
+    assert p.modules[e.address].name == 'exceptions'
+    assert p.memory.describe_address(e.address).startswith("exceptions:do_access_read_violation")
+    assert isinstance(e.backtrace, frida_util.native_exception.NativeBacktrace)
+    assert isinstance(e.context, frida_util.tracer.contexts.x64.X64Context)
+
+    assert e.memory_address == 0x666
+    assert e.memory_operation == 'read'
+
+    # If we handled exception correctly, process should still be in good state
+    assert not isinstance(do_good, frida_util.native_exception.NativeException)
