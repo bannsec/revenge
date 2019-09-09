63ced885f87885643ecf838f5fa7034cc52a4752
bannsec <whootandahalf@gmail.com>
2019-09-09 20:13:51 +0000

    Java.classes enum -> async and no timeout

--- revenge/java/classes.py
+++ revenge/java/classes.py
@@ -42,7 +42,13 @@ class JavaClasses(object):
         try:
             return self.__classes
         except AttributeError:
-            self.__classes = self._process.java.run_script_generic(r"""send( Java.enumerateLoadedClassesSync() );""", raw=True, unload=True)[0][0]
+            self.__classes = self._process.java.run_script_generic(
+                    r"""send( Java.enumerateLoadedClassesSync() ); send("DONE");""",
+                    raw=True,
+                    unload=True,
+                    timeout=0,
+                    onComplete='DONE',
+                    )[0][0]
             return self.__classes
 
 from .java_class import JavaClass
