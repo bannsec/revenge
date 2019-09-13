96cd24560010c472b7183519f636d7047fba7c53
bannsec <whootandahalf@gmail.com>
2019-09-13 02:20:59 +0000

    Letting Travis sleep on Android...

--- test_android.sh
+++ test_android.sh
@@ -5,6 +5,10 @@ mkdir -p coverage
 # Make sure android is up and running
 while [ "`adb shell getprop init.svc.bootanim`" != "stopped" ]; do sleep 10; done
 
-# Current issues with latest frida server release...
-# android = device_types.AndroidDevice(type='usb', frida_server_release='12.6.11') <-- use this for testing manuall
+# Travis is SLOWWW
+if [ ! -z $TRAVIS ]; then
+    echo "In Travis... Sleeping 5 minutes to let emulator boot."
+    sleep 300
+fi
+
 pytest -v --cov --cov-report=term --cov-report=html $@ tests/android/
