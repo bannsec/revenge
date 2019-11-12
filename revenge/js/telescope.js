8c23f47a1f714aebbd26fb00afa617c93b5fd11e
bannsec <whootandahalf@gmail.com>
2019-11-12 01:19:07 +0000

    Restructure telescope.js

--- revenge/js/telescope.js
+++ revenge/js/telescope.js
@@ -18,8 +18,8 @@ function telescope_get_type(thing) {
 }
 
 
-function telescope(v, telescope_depth, type_hint, mem_range) {
-    
+function telescope(v, telescope_depth, type_hint) {
+
     if ( telescope_depth === undefined ) {
         telescope_depth = 0;
     } else {
@@ -43,35 +43,30 @@ function telescope(v, telescope_depth, type_hint, mem_range) {
     // If we've hit our max depth, return
     if ( telescope_depth >= 3 ) return scope;
 
-    // Use mem_range we're given or find it ourselves
-    if ( mem_range ) {
-        scope.mem_range = mem_range;
+    // Setup mem_range
+    if ( ptr_v.compare(ptr("0xffffff")) < 0 ) {
+        // Assume this isn't a valid range
+        scope.mem_range = null;
     } else {
-        
-        if ( ptr_v.compare(ptr("0xffffff")) < 0 ) {
-            // Assume this isn't a valid range
-            scope.mem_range = null;
-        } else {
-            scope.mem_range = Process.findRangeByAddress(ptr_v);
-        }
+        scope.mem_range = Process.findRangeByAddress(ptr_v);
     }
     
     // Doesn't point anywhere else, we're done
     if ( scope.mem_range === null ) return scope;
 
-
     // If this points to readable memory
-    if ( scope.mem_range.protection[0] === "r" && type_hint !== "instruction") {
+    if ( type_hint !== "instruction") {
 
-        // Try to telescope it as another pointer
-        var mem_next = ptr_v.readPointer();
-        var mem_next_range = Process.findRangeByAddress(mem_next);
-
-        // If this is a valid pointer, recurse down into it
-        if ( mem_next_range !== null ) {
-            scope.next = telescope(mem_next, telescope_depth, null, mem_next_range);
+        try {
+            // Try to telescope it as another pointer
+            var mem_next = ptr_v.readPointer();
+            // This is a little implicit test to determine if it's a pointer to
+            // a pointer. This will except out if it's not a pointer to a
+            // pointer.
+            mem_next.readPointer();
+            scope.next = telescope(mem_next, telescope_depth, null, null);
             return scope;
-        }
+        } catch (e) {}
 
         // Is it a string?
         try {
@@ -80,10 +75,11 @@ function telescope(v, telescope_depth, type_hint, mem_range) {
             if ( as_str.length > 2 ) {
                 scope.next = telescope(as_str, telescope_depth, null);
                 // Keep int in case we messed up..
-                scope.next["int"] = ptr_v.readPointer();
+                scope.next.int = ptr_v.readPointer();
                 return scope;
             }
         } catch (error) {}
+
 
     }
 
@@ -103,6 +99,11 @@ function telescope(v, telescope_depth, type_hint, mem_range) {
     } catch (e) {}
 
     // This is probably a pointer to some number...
-    scope.next = telescope(ptr_v.readPointer(), telescope_depth, null);
+    try {
+        scope.next = telescope(ptr_v.readPointer(), telescope_depth, null);
+        return scope;
+    } catch (e) {};
+
+    // *shrug*
     return scope;
 }
