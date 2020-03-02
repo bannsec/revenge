14070c19180856e3fcfff6ab3a2654019bea6baf
bannsec <whootandahalf@gmail.com>
2020-03-02 21:08:34 +0000

    reverting telescoping on exception until frida updates

--- revenge/js/exception_handler.js
+++ revenge/js/exception_handler.js
@@ -16,7 +16,7 @@ Process.setExceptionHandler(
         details.wait_for = wait_for;
 
         // TODO: Re-enable this once try/catch works inside the exception handler
-        details.context = timeless_snapshot(details, false).context;
+        //details.context = timeless_snapshot(details, false).context;
         send(details);
 
         while ( wait_for.readS8() == 0 ) { Thread.sleep(0.25); };
